20543700922cac44981dea3984ce4d2446e33d8e
curtischatch <curtis.chat.hk@gmail.com>
2018-08-10 14:39:38 -0400

    crud actions for restfuldic pt2

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,17 +1,17 @@
 class EntriesController < ApplicationController
 
   def index
-    # here we'll define some @instance_variables to store data from the database for the views to use
     @entries = Entry.all
     render :index
+    # here we'll define some @instance_variables to store data from the database for the views to use
   end
 
   def show
-    render :show
+    @entry = Entry.find{params[:id]}
   end
 
   def new
-    render :new
+    @entry = Entry.new
   end
 
   def create
@@ -19,11 +19,20 @@ class EntriesController < ApplicationController
   end
 
   def edit
-    render :edit
+    @entry = Entry.find(params[:id])
   end
 
   def update
     redirect_to entry_url(params[:id])
+    @entry = Entry.new
+    @entry.word = params[:entry][:word]
+    @entry.language = params[:entry][:language]
+    @entry.definition = params[:entry][:definition]
+    if @entry.save
+      redirect_to entries_url
+    else
+      render :new
+    end
   end
 
   def destroy
