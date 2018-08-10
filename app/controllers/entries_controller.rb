c2180ebff26663cbcc7193e916463a645f41d7b7
curtischatch <curtis.chat.hk@gmail.com>
2018-08-10 12:01:54 -0400

    completed restful dictionary

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,7 +2,7 @@ class EntriesController < ApplicationController
 
   def index
     # here we'll define some @instance_variables to store data from the database for the views to use
-    render :index
+    # render :index
   end
 
   def show
